a97b48bc2f0d67f713f04a08c8db72232da48410
Taufique <iradukunda.ta@gmail.com>
2019-08-07 19:29:04 +0200

    add userdashboard

--- accounts/urls.py
+++ accounts/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 
 from django.urls import path
 from accounts import views
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 urlpatterns = [
     path('', views.indexView, name='home'),
     path('dashboard/', views.dashboardView, name='dashboard'),
     path('login/', LoginView.as_view(), name='login_url'),
     path('register/', views.registerView, name='register_url'),
-    # path('logout/',),
+    path('logout/',LogoutView.as_view(next_page='dashboard'), name = 'logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render, redirect
-
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
 def indexView(request):
     return render(request, 'index.html')
-
+@login_required
 def dashboardView(request):
-    return  render(request, 'dashboard.html')
+    return  render(request, 'index1.html')
 
 def registerView(request):
 
